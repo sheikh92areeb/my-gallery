25f17858681322b18a7e40baa296c4ad868e2b73
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-09-01 19:59:37 +0500

    working

---
+++ reactjs/src/context/ProjectContext.tsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState, type FC, type ReactNode } from "react";
+
+type Category = {
+  id: string;
+  categoryName: string;
+};
+
+type Project = {
+  id: string;
+  title: string;
+  short_description: string;
+  description: string;
+  category: string;
+  tags: string[];
+  features: string[];
+  techStack: string[];
+  images: string[];
+};
+
+type ProjectContextType = {
+  categories: Category[];
+  projects: Project[];
+  loading: boolean;
+  error: string | null;
+};
+
+const ProjectContext = createContext<ProjectContextType | undefined>(undefined);
+
+export const ProjectProvider: FC<{children: ReactNode}> = ({children}) => {
+    const [categories, setCategories] = useState<Category[]>([])
+    const [projects, setProjects] = useState<Project[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
+
+    async function fetchData() {
+        try {
+            setLoading(true);
+            const res = await fetch("https://sheikh92areeb.github.io/myFakeDB/ProjectGallery/projects.json");
+            if (!res.ok) throw new Error("Failed to fetch data");
+
+            const data = await res.json();
+
+            setCategories(data.category || []);
+            setProjects(data.projects || []);
+
+        } catch (err: any) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const value = { categories, projects, loading, error }
+
+    return (
+        <ProjectContext.Provider value={value}>
+            {children}
+        </ProjectContext.Provider>
+    )
+}
+
+export const useProjects = (): ProjectContextType => {
+    const context = useContext(ProjectContext);
+    if (!context) {
+        throw new Error("useProjects must be used within a ProjectProvider");
+    }
+    return context;
+};
--- reactjs/src/main.tsx
+++ reactjs/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { ProjectProvider } from './context/ProjectContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ProjectProvider>
+        <App />
+      </ProjectProvider>     
     </BrowserRouter>
   </StrictMode>,
 )
